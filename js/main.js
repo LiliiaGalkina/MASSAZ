709b79e6eb636c601366d302299a9598daf4227b
LiliiaGalkina <154988375+LiliiaGalkina@users.noreply.github.com>
2023-12-27 21:51:30 +0300

    Клиника здоровья

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let burger = document.querySelector('#burger');
+let menu = document.querySelector('#menu');
+
+burger.addEventListener('click', func);
+
+function func() {
+	burger.classList.toggle('active');
+	menu.classList.toggle('active');
+}
+
+//-------------------------------------------
+
+let blocks = document.querySelectorAll('#block');
+let buttons = document.querySelectorAll('#btn');
+
+
+
+for(let i = 0; i < buttons.length; i++){
+	buttons[i].addEventListener('click', function(){
+		blocks[i].classList.toggle('open');
+	})
+}
